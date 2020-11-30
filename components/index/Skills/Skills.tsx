af38b9f96226e78f7a778f7c2b290802c0ab1520
Zak <zakkempson@gmail.com>
2020-11-30 16:17:59 +0000

    Update skills descriptions.

--- components/index/Skills/Skills.tsx
+++ components/index/Skills/Skills.tsx
@@ -13,25 +13,24 @@ const skills: SkillI[] = [
     title: 'Research',
     icon: 'research',
     description:
-      'No.1 reason for product failure is not making what users want.  My first step is to deeply understand the humans we are building for, using existing info or by conducting new UX research.',
+      'The No.1 reason for product failure is not making what people want.  My first step is to deeply understand the humans we are building for, using existing info or by with new research.',
   },
   {
     title: 'Business',
     icon: 'business',
     description:
-      'No.1 reason for product failure is not making what users want.  My first step is to deeply understand the humans we are building for, using existing info or by conducting new UX research.',
+      'The best tool we have created (so far) for harnessing human ambition for good is capitalism. For a design to do good, it must be economical as well as functional and beautuful.',
   },
   {
     title: 'Design',
     icon: 'design',
     description:
-      'No.1 reason for product failure is not making what users want.  My first step is to deeply understand the humans we are building for, using existing info or by conducting new UX research.',
+      'For me, design is a synthesis of human needs & behaviours, STEM fields, art and economics. I learn deeply about each ingredient in order to find more useful combinations.',
   },
   {
     title: 'Build',
     icon: 'build',
-    description:
-      'No.1 reason for product failure is not making what users want.  My first step is to deeply understand the humans we are building for, using existing info or by conducting new UX research.',
+    description: `Should designers code? In my experience, understanding how designs will be brought to life helps to bring product teams closer and results in a better end product.`,
   },
 ];
 
